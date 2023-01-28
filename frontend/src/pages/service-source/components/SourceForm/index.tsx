700c999523d07947b7b72c8c6a0dec34b6aa9ee7
Risker-C <41248085+Risker-C@users.noreply.github.com>
2023-01-28 14:24:35 +0800

    完成服务来源页面绘制 (#33)

---
+++ frontend/src/pages/service-source/components/SourceForm/index.tsx
@@ -0,0 +1,177 @@
+import React, { useEffect, useState, forwardRef, useImperativeHandle } from 'react';
+import { Form, Input, Select } from 'antd';
+
+const { Option } = Select;
+
+/**
+ * Service Source Type Enum
+ */
+const SourceType = {
+  Default: "default",
+  Nacos2: "nacos2",
+  Nacos: "nacos",
+  Zookeeper: "zookeeper",
+  Consul: "consul",
+  Eureka: "eureka"
+};
+
+const SourceForm: React.FC = forwardRef((props, ref) => {
+
+  const { value } = props;
+  const [form] = Form.useForm();
+  const [sourceType, setSourceType] = useState<string>(SourceType.Default);
+
+  useEffect(() => {
+    form.resetFields();
+    if (value) {
+      setSourceType(value.type);
+      form.setFieldsValue(value);
+    }
+  }, [value]);
+
+  useImperativeHandle(ref, () => ({
+    reset: () => {
+      setSourceType(SourceType.Default)
+      form.resetFields();
+    },
+    handleSubmit: () => (form.validateFields()),
+  }));
+
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+    >
+      <Form.Item
+        label="注册中心类型"
+        required
+        name='type'
+        tooltip="注册中心类型，可选项：nacos,nacos2.x,zookeeper"
+      >
+        <Select
+          allowClear
+          placeholder="请选择类型"
+          onChange={(v) => setSourceType(v)}
+        >
+          <Option value="nacos2">nacos2.x</Option>
+          <Option value="nacos">nacos</Option>
+          <Option value="zookeeper">zookeeper</Option>
+          {/* <Option value="consul">consul</Option>
+          <Option value="eureka">eureka</Option> */}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="服务来源名称"
+        required
+        name='name'
+        rules={[
+          {
+            required: true,
+            message: '支持大小写字母、数字、下划线（_）、短划线（-）和星号（*），不超过256个字符。',
+          },
+        ]}
+      >
+        <Input
+          showCount
+          allowClear
+          maxLength={256}
+          placeholder="支持大小写字母、数字、下划线（_）、短划线（-）和星号（*），不超过256个字符。"
+        />
+      </Form.Item>
+      <Form.Item
+        label="注册中心地址"
+        required
+        name='domain'
+        tooltip="注册中心地址，可以是ip或域名"
+        rules={[
+          {
+            required: true,
+            message: '注册中心地址，可以是ip或域名',
+          },
+        ]}
+      >
+        <Input
+          showCount
+          allowClear
+          maxLength={256}
+          placeholder="支持大小写字母、数字、下划线（_）、短划线（-）和星号（*），不超过256个字符。"
+        />
+      </Form.Item>
+      <Form.Item
+        label="注册中心访问端口"
+        required
+        name='port'
+        rules={[
+          {
+            required: true,
+            message: '注册中心访问端口',
+          },
+        ]}
+      >
+        <Input
+          allowClear
+          placeholder="注册中心访问端口"
+        />
+      </Form.Item>
+
+      {
+        sourceType === SourceType.Zookeeper ? (
+          <div>
+            <Form.Item
+              label="服务注册根路径"
+              name='zkServicesPath'
+              tooltip="使用zk时,填写服务注册的根路径,默认监听 /dubbo 和 /services，前者为dubbo 服务默认根路径，后者为SpringCloud服务默认根路径"
+            >
+              <Select
+                allowClear
+                mode="tags"
+                placeholder="默认监听 /dubbo 和 /services，前者为dubbo 服务默认根路径，后者为SpringCloud服务默认根路径"
+                options={[]}
+              />
+            </Form.Item>
+          </div>
+        ) : (sourceType === SourceType.Nacos || sourceType === SourceType.Nacos2) ? (<div>
+          <Form.Item
+            label="nacos命名空间id"
+            name='nacosNamespaceId'
+          >
+            <Input
+              showCount
+              allowClear
+              maxLength={256}
+              placeholder="支持大小写字母、数字、下划线（_）、短划线（-）和星号（*），不超过256个字符。"
+            />
+          </Form.Item>
+          <Form.Item
+            label="nacos服务分组列表"
+            name='nacosGroups'
+          >
+            <Select
+              mode="tags"
+              allowClear
+              options={[{ value: "DEFAULT_GROUP" }]}
+            />
+          </Form.Item>
+        </div>) : sourceType === SourceType.Consul ? (
+          <div>
+            <Form.Item
+              label="命名空间"
+              name='consulNamespace'
+              tooltip="使用consul时需要填写命名空间"
+            >
+              <Input
+                showCount
+                allowClear
+                maxLength={256}
+                placeholder="consul命名空间"
+              />
+            </Form.Item>
+          </div>
+        ) : null
+      }
+    </Form>
+  );
+});
+
+export default SourceForm;
--- frontend/src/pages/service-source/index.tsx
+++ frontend/src/pages/service-source/index.tsx
@@ -1,15 +1,198 @@
-import { Card, Result } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Table, Col, Form, Row, Button, Modal, Space, Drawer } from 'antd';
+import { useRequest } from 'ahooks';
+import { RedoOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import { SourceFormProps, SourceFormRef, SourceItem, SourceResponse } from '@/interfaces/source';
+import { addServiceSources, deleteServiceSources, getServiceSources, updateServiceSources } from '@/services/source';
+import { uniqueId } from "lodash";
+import SourceForm from './components/SourceForm';
 import { useTranslation } from 'react-i18next';
 
-export default function SourcePage() {
+const SourceList: React.FC = () => {
   const { t } = useTranslation();
+  const columns = [
+    {
+      title: '注册中心类型',
+      dataIndex: 'type',
+      key: 'type',
+      ellipsis: true,
+    },
+    {
+      title: '服务来源名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '注册中心地址',
+      dataIndex: 'domain',
+      key: 'domain',
+    },
+    {
+      title: '注册中心访问端口',
+      dataIndex: 'port',
+      key: 'port',
+    },
+    {
+      title: '操作',
+      dataIndex: 'action',
+      key: 'action',
+      width: 140,
+      align: 'center',
+      render: (_, record) => (
+        <Space size="small">
+          <a onClick={() => onEditDrawer(record)}>编辑</a>
+          <a onClick={() => onShowModal(record)}>删除</a>
+        </Space>
+      ),
+    },
+  ];
+
+  const [form] = Form.useForm();
+  const formRef = useRef<SourceFormRef>(null);
+  const [dataSource, setDataSource] = useState<SourceItem[]>([]);
+  const [currentDomain, setCurrentDomain] = useState<SourceItem | null>();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const getDomainList = async (factor): Promise<SourceResponse> => (getServiceSources(factor));
+
+  const { loading, run, refresh } = useRequest(getDomainList, {
+    manual: true,
+    onSuccess: (result, params) => {
+      const { data: _dataSource } = result;
+      _dataSource.forEach(i => {
+        i.key || (i.key = i.id || (i.name + '_' + i.type))
+      })
+      setDataSource(_dataSource);
+    },
+  });
+
+  useEffect(() => {
+    run({});
+  }, []);
+
+  const onEditDrawer = (domain: SourceItem) => {
+    setCurrentDomain(domain);
+    setOpenDrawer(true);
+  };
+
+  const onShowDrawer = () => {
+    setOpenDrawer(true);
+    setCurrentDomain(null);
+  };
+
+  const handleDrawerOK = async () => {
+    try {
+      const values: SourceFormProps = formRef.current ? await formRef.current.handleSubmit() : {} as SourceFormProps;
+
+      if (currentDomain) {
+        const _id = currentDomain.id || parseInt(uniqueId(), 10);
+        await updateServiceSources({ id: _id, ...values } as SourceItem);
+      } else {
+        await addServiceSources(values as SourceItem);
+      }
+
+      setOpenDrawer(false);
+      formRef.current && formRef.current.reset();
+      refresh();
+
+    } catch (errInfo) {
+      console.log('Save failed:', errInfo);
+    }
+  };
+
+  const handleDrawerCancel = () => {
+    setOpenDrawer(false);
+    formRef.current && formRef.current.reset();
+    setCurrentDomain(null);
+  };
+
+  const onShowModal = (domain: SourceItem) => {
+    setCurrentDomain(domain);
+    setOpenModal(true);
+  };
+
+  const handleModalOk = async () => {
+    setConfirmLoading(true);
+    await deleteServiceSources({ name: currentDomain?.name });
+    setConfirmLoading(false);
+    setOpenModal(false);
+    // 重新刷新
+    refresh();
+  };
+
+
+  const handleModalCancel = () => {
+    setOpenModal(false);
+    setCurrentDomain(null);
+  };
+
   return (
-    <Card bordered={false}>
-      <Result
-        status="403"
-        title=""
-        subTitle={t('misc.tbd')}
+    <PageContainer>
+      <Form
+        form={form}
+        style={{
+          background: '#fff',
+          height: 64,
+          paddingTop: 16,
+          marginBottom: 16,
+          paddingLeft: 16,
+          paddingRight: 16,
+        }}
+      >
+        <Row gutter={24}>
+          <Col span={4}>
+            <Button
+              type="primary"
+              onClick={onShowDrawer}
+            >
+              创建服务来源
+            </Button>
+          </Col>
+          <Col span={20} style={{ textAlign: 'right' }}>
+            <Button
+              icon={<RedoOutlined />}
+              onClick={refresh}
+            />
+          </Col>
+        </Row>
+      </Form>
+      <Table
+        loading={loading}
+        dataSource={dataSource}
+        columns={columns}
+        pagination={false}
       />
-    </Card>
+      <Drawer
+        title="创建服务来源"
+        placement='right'
+        width={660}
+        onClose={handleDrawerCancel}
+        open={openDrawer}
+        extra={
+          <Space>
+            <Button onClick={handleDrawerCancel}>取消</Button>
+            <Button type="primary" onClick={handleDrawerOK}>
+              确定
+            </Button>
+          </Space>
+        }
+      >
+        <SourceForm ref={formRef} value={currentDomain} />
+      </Drawer>
+      <Modal
+        title={<div><ExclamationCircleOutlined style={{ color: '#ffde5c', marginRight: 8 }} />删除</div>}
+        open={openModal}
+        onOk={handleModalOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleModalCancel}
+      >
+        <p>确定删除 <span style={{ color: '#0070cc' }}>{(currentDomain && currentDomain.name) || ''} </span>吗？</p>
+      </Modal>
+    </PageContainer>
   );
-}
+};
+
+export default SourceList;
