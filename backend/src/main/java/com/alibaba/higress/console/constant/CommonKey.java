2662f4b6757dcf902a5894b1837ff73e7a2406b7
Kent Dong <ch3cho@qq.com>
2023-02-02 10:15:55 +0800

    Update config keys to avoid conflicts with K8s injected envs (#42)

--- backend/src/main/java/com/alibaba/higress/console/constant/CommonKey.java
+++ backend/src/main/java/com/alibaba/higress/console/constant/CommonKey.java
@@ -4,26 +4,28 @@ import java.nio.file.Paths;
 
 public class CommonKey {
 
-    public final static String HIGRESS_KUBE_CONFIG_KEY = "higress.kube-config";
+    public final static String CONFIG_KEY_PREFIX = "higress-console.";
 
-    public final static String HIGRESS_KUBE_CONFIG_DEFAULT_PATH = Paths.get(System.getProperty("user.home"), "/.kube/config").toString();
+    public final static String KUBE_CONFIG_KEY = CONFIG_KEY_PREFIX + "kube-config";
 
-    public final static String HIGRESS_NS_KEY = "higress.ns";
+    public final static String KUBE_CONFIG_DEFAULT_PATH = Paths.get(System.getProperty("user.home"), "/.kube/config").toString();
 
-    public final static String HIGRESS_NS_DEFAULT = "higress-system";
+    public final static String NS_KEY = CONFIG_KEY_PREFIX + "ns";
 
-    public final static String HIGRESS_CONTROLLER_SERVICE_NAME_KEY = "higress.controller.service.name";
+    public final static String NS_DEFAULT = "higress-system";
 
-    public final static String HIGRESS_CONTROLLER_SERVICE_NAME_DEFAULT = "higress-controller";
+    public final static String CONTROLLER_SERVICE_NAME_KEY = CONFIG_KEY_PREFIX + "controller.service.name";
 
-    public final static String HIGRESS_CONTROLLER_SERVICE_HOST_KEY = "higress.controller.service.host";
+    public final static String CONTROLLER_SERVICE_NAME_DEFAULT = "higress-controller";
 
-    public final static String HIGRESS_CONTROLLER_SERVICE_HOST_DEFAULT = "localhost";
+    public final static String CONTROLLER_SERVICE_HOST_KEY = CONFIG_KEY_PREFIX + "controller.service.host";
 
-    public final static String HIGRESS_CONTROLLER_SERVICE_PORT_KEY = "higress.controller.service.port";
+    public final static String CONTROLLER_SERVICE_HOST_DEFAULT = "localhost";
 
-    public final static int HIGRESS_CONTROLLER_SERVICE_PORT_DEFAULT = 15014;
+    public final static String CONTROLLER_SERVICE_PORT_KEY = CONFIG_KEY_PREFIX + "controller.service.port";
 
-    public final static String HIGRESS_CONTROLLER_ACCESS_TOKEN_KEY = "higress.controller.access-token";
+    public final static int CONTROLLER_SERVICE_PORT_DEFAULT = 15014;
+
+    public final static String CONTROLLER_ACCESS_TOKEN_KEY = CONFIG_KEY_PREFIX + "controller.access-token";
 
 }
