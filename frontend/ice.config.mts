72590629a07b20496bab14c71e175ee2ae504ff0
songpengyuan <turingmaster@163.com>
2023-02-14 14:47:51 +0800

    hotfix: Solve the static resource caching problem bug

--- frontend/ice.config.mts
+++ frontend/ice.config.mts
@@ -1,29 +1,26 @@
-import { defineConfig } from '@ice/app';
-import request from '@ice/plugin-request';
-import store from '@ice/plugin-store';
-import auth from '@ice/plugin-auth';
+import { defineConfig } from "@ice/app";
+import request from "@ice/plugin-request";
+import store from "@ice/plugin-store";
+import auth from "@ice/plugin-auth";
 
 // The project config, see https://v3.ice.work/docs/guide/basic/config
 export default defineConfig(() => ({
   ssr: false,
   ssg: false,
+  hash: "contenthash",
   routes: {
     defineRoutes: (route) => {
-      if (process.platform !== 'win32') {
-        route('*', '404.tsx');
+      if (process.platform !== "win32") {
+        route("*", "404.tsx");
       }
     },
   },
   proxy: {
-    '/api': {
-      target: 'http://demo.higress.io/',
+    "/api": {
+      target: "http://demo.higress.io/",
       changeOrigin: true,
-      pathRewrite: { '^/api' : '' },
+      pathRewrite: { "^/api": "" },
     },
   },
-  plugins: [
-    request(),
-    store(),
-    auth(),
-  ],
+  plugins: [request(), store(), auth()],
 }));
--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+import translation_en from "@/locales/en-US/translation.json";
+import translation_zh from "@/locales/zh-CN/translation.json";
 
 i18n
   .use(HttpApi)
@@ -13,11 +15,29 @@ i18n
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
+    resources: {
+      en: {
+        translation: translation_en,
+      },
+      zh: {
+        translation: translation_zh,
+      },
+    },
   });
 
 export default i18n;
 
 export const lngs = [
-  { code: "zh-CN", nativeName: "简体中文", switchText: "中", officialSiteCode: "zh-cn" },
-  { code: "en-US", nativeName: "English (United States)", switchText: "En", officialSiteCode: "en-us" },
+  {
+    code: "zh-CN",
+    nativeName: "简体中文",
+    switchText: "中",
+    officialSiteCode: "zh-cn",
+  },
+  {
+    code: "en-US",
+    nativeName: "English (United States)",
+    switchText: "En",
+    officialSiteCode: "en-us",
+  },
 ];
