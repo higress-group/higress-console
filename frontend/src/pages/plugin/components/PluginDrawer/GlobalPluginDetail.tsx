21bef7663d6434e84509b8df44d31c5c7ffcedaf
songpengyuan <turingmaster@163.com>
2023-04-06 19:38:31 +0800

    fix: add route plugin get/update api

--- frontend/src/pages/plugin/components/PluginDrawer/GlobalPluginDetail.tsx
+++ frontend/src/pages/plugin/components/PluginDrawer/GlobalPluginDetail.tsx
@@ -1,9 +1,10 @@
 import CodeEditor from '@/components/CodeEditor';
-import React, { useState, useImperativeHandle, useEffect, forwardRef } from 'react';
-import { Form, message, Divider, Switch, Spin } from 'antd';
+import React, { useState, useImperativeHandle, useEffect, forwardRef, useMemo } from 'react';
+import { Form, message, Divider, Switch, Spin, Alert } from 'antd';
 
-import { getWasmPluginsConfig, updateGlobalPluginInstances, getGlobalPluginInstances } from '@/services';
+import * as servicesApi from '@/services';
 import { useRequest } from 'ahooks';
+import { useSearchParams } from 'ice';
 
 export interface IPluginData {
   configurations: object;
@@ -18,6 +19,7 @@ export interface IPluginData {
 
 export interface IPropsData {
   name?: string;
+  category: string;
 }
 export interface IProps {
   data: IPropsData;
@@ -26,7 +28,33 @@ export interface IProps {
 
 const GlobalPluginDetail = forwardRef((props: IProps, ref) => {
   const { data, onSuccess } = props;
-  const { name = '' } = data || {};
+  const { name: pluginName = '' } = data || {};
+
+  const [searchParams] = useSearchParams();
+
+  const queryType: string = searchParams.get('type') || '';
+  const queryName: string = searchParams.get('name') || '';
+
+  const isChangeExampleRaw = useMemo(() => {
+    return ['route', 'domain'].includes(queryType) && data.category === 'auth';
+  }, [queryType]);
+
+  const isRoutePlugin = useMemo(() => {
+    return queryType === 'route';
+  }, [queryType]);
+
+  const pluginInstancesApi = useMemo(() => {
+    if (queryType === 'route') {
+      return {
+        get: servicesApi.getRoutePluginInstances.bind(servicesApi),
+        update: servicesApi.updateRoutePluginInstances.bind(servicesApi),
+      };
+    }
+    return {
+      get: servicesApi.getGlobalPluginInstances.bind(servicesApi),
+      update: servicesApi.updateGlobalPluginInstances.bind(servicesApi),
+    };
+  }, [queryType, queryName]);
 
   const [form] = Form.useForm();
 
@@ -36,31 +64,34 @@ const GlobalPluginDetail = forwardRef((props: IProps, ref) => {
   const [rawConfigurations, setRawConfigurations] = useState('');
   const [defaultValue, setDefaultValue] = useState('');
 
-  const { loading: getDataLoading, run: getData } = useRequest(getGlobalPluginInstances, {
+  const { loading: getDataLoading, run: getData } = useRequest(pluginInstancesApi.get, {
     manual: true,
-    onSuccess: (res) => {
+    onSuccess: (res: IPluginData) => {
       setPluginData(res);
       setRawConfigurations(res.rawConfigurations);
       setDefaultValue(res.rawConfigurations);
-      getConfig(name);
+      getConfig(pluginName);
     },
   });
-  const { loading: getConfigLoading, run: getConfig } = useRequest(getWasmPluginsConfig, {
+  const { loading: getConfigLoading, run: getConfig } = useRequest(servicesApi.getWasmPluginsConfig, {
     manual: true,
     onSuccess: (res) => {
       setConfigData(res);
       if (!defaultValue) {
-        const exampleRaw = res?.schema.exampleRaw;
+        let exampleRaw = res?.schema.exampleRaw;
+        if (isChangeExampleRaw) {
+          exampleRaw = 'allow:[]';
+        }
         setRawConfigurations(exampleRaw);
         setDefaultValue(exampleRaw);
       }
       form.setFieldsValue({
-        enabled: pluginData.enabled,
+        enabled: pluginData?.enabled,
       });
     },
   });
 
-  const { loading: updateLoading, run: updateData } = useRequest(updateGlobalPluginInstances, {
+  const { loading: updateLoading, run: updateData } = useRequest(pluginInstancesApi.update, {
     manual: true,
     onSuccess: () => {
       onSuccess();
@@ -72,16 +103,38 @@ const GlobalPluginDetail = forwardRef((props: IProps, ref) => {
     await form.validateFields();
     const values = form.getFieldsValue();
 
-    updateData(name, {
+    const params = {
       ...pluginData,
       enabled: values.enabled,
       rawConfigurations,
-    });
+    };
+
+    delete params.configurations;
+
+    if (isRoutePlugin) {
+      updateData(
+        {
+          routeName: queryName,
+          pluginName,
+        },
+        params,
+      );
+      return;
+    }
+
+    updateData(pluginName, params);
   };
 
   useEffect(() => {
-    getData(name);
-  }, [name]);
+    if (isRoutePlugin) {
+      getData({
+        routeName: queryName,
+        pluginName,
+      });
+      return;
+    }
+    getData(pluginName);
+  }, [pluginName, queryName]);
 
   useImperativeHandle(ref, () => ({
     submit: onSubmit,
@@ -90,6 +143,9 @@ const GlobalPluginDetail = forwardRef((props: IProps, ref) => {
   return (
     <div>
       <Spin spinning={getConfigLoading || getDataLoading || updateLoading}>
+        {isRoutePlugin && queryName && (
+          <Alert style={{ marginBottom: '10px' }} message={`作用路由： ${queryName}`} type="warning" showIcon />
+        )}
         <Form name="basic" form={form} autoComplete="off">
           <Form.Item label="开启状态" name="enabled" valuePropName="checked">
             <Switch />
