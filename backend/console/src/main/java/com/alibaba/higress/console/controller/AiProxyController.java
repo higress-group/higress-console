76db109b9dbc78038a87b618596842c03901af50
CH3CHO <ch3cho@qq.com>
2024-06-24 11:32:55 +0800

    Fix up

--- backend/console/src/main/java/com/alibaba/higress/console/controller/AiProxyController.java
+++ backend/console/src/main/java/com/alibaba/higress/console/controller/AiProxyController.java
@@ -19,7 +19,6 @@ import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
