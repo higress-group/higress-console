ce24afa37e45a2abc9576b0e7532f0493514aec7
Kent Dong <ch3cho@qq.com>
2025-05-15 15:08:19 +0800

    Update SystemController.java

--- backend/console/src/main/java/com/alibaba/higress/console/controller/SystemController.java
+++ backend/console/src/main/java/com/alibaba/higress/console/controller/SystemController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.higress.console.constant.UserConfigKey;
