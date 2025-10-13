0a1935ddfc85216186c3b7304da5da67cb211c02
lcfang <licuifangli@126.com>
2025-10-13 20:52:44 +0800

    [vport]使用empty组件优化自定义服务端口暂无数据时的页面展示

--- frontend/src/pages/service-source/components/SourceForm/index.tsx
+++ frontend/src/pages/service-source/components/SourceForm/index.tsx
@@ -6,7 +6,7 @@ import { getServiceSourceTypeConfig, isNacosType, ServiceProtocols, ServiceSourc
 import { getGatewayRoutes } from '@/services';
 import { getProxyServers } from '@/services/proxy-server';
 import { DeleteOutlined, PlusOutlined, QuestionCircleOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Select, Tooltip } from 'antd';
+import { Button, Empty, Form, Input, Select, Tooltip } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import { useRequest } from 'ice';
 import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
@@ -564,48 +564,11 @@ const SourceForm: React.FC = forwardRef((props, ref) => {
                                 ) : (
                                   <div style={{
                                     padding: '20px 0',
-                                    textAlign: 'center',
-                                    color: '#8c8c8c',
                                     borderLeft: 'none',
                                     borderRight: 'none',
                                   }}
                                   >
-                                    <div style={{ marginBottom: '4px' }}>
-                                      <svg
-                                        width="32"
-                                        height="20"
-                                        viewBox="0 0 64 41"
-                                        xmlns="https://www.w3.org/2000/svg"
-                                      >
-                                        <g transform="translate(0 1)" fill="none" fillRule="evenodd">
-                                          <ellipse
-                                            fill="#f5f5f5"
-                                            cx="32"
-                                            cy="33"
-                                            rx="32"
-                                            ry="7"
-                                          />
-                                          <g fillRule="nonzero" stroke="#d9d9d9">
-                                            <path
-                                              d={
-                                                "M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093" +
-                                                "c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"
-                                              }
-                                            />
-                                            <path
-                                              d={
-                                                "M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1" +
-                                                "C10.32 35 9 33.259 9 31.137V13h11.16" +
-                                                "c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752" +
-                                                "c1.232 0 2.237-1.308 2.237-2.913v-.007z"
-                                              }
-                                              fill="#fafafa"
-                                            />
-                                          </g>
-                                        </g>
-                                      </svg>
-                                    </div>
-                                    <label>{t('serviceSource.serviceSourceForm.vportServiceNull')}</label>
+                                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
                                   </div>
                                 )}
                               </div>
