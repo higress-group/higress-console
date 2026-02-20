e625b7cec04cc1e64e1ff9b29024280ec70e9882
澄潭 <zty98751@alibaba-inc.com>
2026-02-20 23:30:38 +0800

    feat: add pluginImageRegistry and pluginImageNamespace config for built-in plugins (#666)

--- backend/sdk/src/main/java/com/alibaba/higress/sdk/model/wasmplugin/WasmPluginServiceConfig.java
+++ backend/sdk/src/main/java/com/alibaba/higress/sdk/model/wasmplugin/WasmPluginServiceConfig.java
@@ -23,6 +23,10 @@ import lombok.Data;
 public class WasmPluginServiceConfig {
     private static final String CUSTOM_IMAGE_URL_PATTERN_ENV = "HIGRESS_ADMIN_WASM_PLUGIN_CUSTOM_IMAGE_URL_PATTERN";
     private static final String CUSTOM_IMAGE_URL_PATTERN_PROPERTY = "higress-admin.wasmplugin.custom-image-url-pattern";
+    private static final String PLUGIN_IMAGE_REGISTRY_ENV = "HIGRESS_ADMIN_WASM_PLUGIN_IMAGE_REGISTRY";
+    private static final String PLUGIN_IMAGE_REGISTRY_PROPERTY = "higress-admin.wasmplugin.plugin-image-registry";
+    private static final String PLUGIN_IMAGE_NAMESPACE_ENV = "HIGRESS_ADMIN_WASM_PLUGIN_IMAGE_NAMESPACE";
+    private static final String PLUGIN_IMAGE_NAMESPACE_PROPERTY = "higress-admin.wasmplugin.plugin-image-namespace";
     private static final String CUSTOM_IMAGE_PULL_SECRET_ENV = "HIGRESS_ADMIN_WASM_PLUGIN_IMAGE_PULL_SECRET";
     private static final String CUSTOM_IMAGE_PULL_POLICY_ENV = "HIGRESS_ADMIN_WASM_PLUGIN_IMAGE_PULL_POLICY";
     private static final String CUSTOM_IMAGE_PULL_SECRET_PROPERTY = "higress-admin.wasmplugin.custom-image-pull-secret";
@@ -32,6 +36,10 @@ public class WasmPluginServiceConfig {
         WasmPluginServiceConfig result = new WasmPluginServiceConfig();
         result.customImageUrlPattern =
             EnvReadUtil.loadCustomConfFromEnv(CUSTOM_IMAGE_URL_PATTERN_PROPERTY, CUSTOM_IMAGE_URL_PATTERN_ENV);
+        result.pluginImageRegistry =
+            EnvReadUtil.loadCustomConfFromEnv(PLUGIN_IMAGE_REGISTRY_PROPERTY, PLUGIN_IMAGE_REGISTRY_ENV);
+        result.pluginImageNamespace =
+            EnvReadUtil.loadCustomConfFromEnv(PLUGIN_IMAGE_NAMESPACE_PROPERTY, PLUGIN_IMAGE_NAMESPACE_ENV);
         result.imagePullSecret =
             EnvReadUtil.loadCustomConfFromEnv(CUSTOM_IMAGE_PULL_SECRET_PROPERTY, CUSTOM_IMAGE_PULL_SECRET_ENV);
         result.imagePullPolicy =
@@ -40,6 +48,8 @@ public class WasmPluginServiceConfig {
     }
 
     private String customImageUrlPattern;
+    private String pluginImageRegistry;
+    private String pluginImageNamespace;
     private String imagePullSecret;
     private String imagePullPolicy;
 }
