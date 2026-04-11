d5fdceacdfe802dfa84498a1374b05897e667813
CH3CHO <ch3cho@qq.com>
2026-04-11 13:38:57 +0800

    feat: Add MACOS_COMPATIBLE env to support running with legacy container engines and Apple Silicon

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-echo $1
+echo "$1"
+if [ "$(uname -m)" = "arm64" ]  && ([ "$MACOS_COMPATIBLE" = "true" ] || [ "$MACOS_COMPATIBLE" = "1" ])
+then
+    JVM_ARGS="$JVM_ARGS -XX:+UnlockDiagnosticVMOptions -XX:-UseAESCTRIntrinsics -XX:UseSVE=0"
+fi
+echo "JVM_ARGS=$JVM_ARGS"
 if [ "$1" = "--local" ]
 then
     java $JVM_ARGS -jar ./console/target/higress-console.jar
