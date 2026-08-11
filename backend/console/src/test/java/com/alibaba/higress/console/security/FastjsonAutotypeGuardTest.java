78c3f36481eeea0dc27646041127380f85e580ae
CH3CHO <ch3cho@qq.com>
2026-08-11 22:52:15 +0800

    style: align fastjson guard test with project license template
    - Update license header from 2022-2026 to 2022-2024 to match the
      project's copyright template.
    - Add trailing newline at end of file (POSIX text-file convention).
    
    Signed-off-by: CH3CHO <ch3cho@qq.com>

--- backend/console/src/test/java/com/alibaba/higress/console/security/FastjsonAutotypeGuardTest.java
+++ backend/console/src/test/java/com/alibaba/higress/console/security/FastjsonAutotypeGuardTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2026 Alibaba Group Holding Ltd.
+ * Copyright (c) 2022-2024 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
